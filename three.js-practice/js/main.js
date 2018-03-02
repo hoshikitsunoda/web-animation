5f6748b685086d44b8d471d37de60297e2497740
seiki tsunoda <liquidohr@gmail.com>
2018-03-02 13:38:45 -0800

    js moved to js folder

--- three.js-practice/js/main.js
+++ three.js-practice/js/main.js
@@ -6,11 +6,17 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 var geometry = new THREE.BoxGeometry(2, 1, 3);
+var geometry2 = new THREE.BoxGeometry(1, 4, 1);
 var material = new THREE.MeshBasicMaterial({
   color: 0xff0000
 });
+var material2 = new THREE.MeshBasicMaterial({
+  color: 0xDAA520
+});
 var cube = new THREE.Mesh(geometry, material);
+var cube2 = new THREE.Mesh(geometry2, material2);
 scene.add(cube);
+scene.add(cube2);
 
 camera.position.z = 8;
 
@@ -18,6 +24,8 @@ function animate() {
   requestAnimationFrame(animate);
   cube.rotation.x += 0.05;
   cube.rotation.y += 0.05;
+  cube2.rotation.x += 0.08;
+  cube2.rotation.y += 0.09;
   renderer.render(scene, camera);
 }
 animate();
